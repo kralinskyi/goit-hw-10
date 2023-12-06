20501f085277c453be48f490ccf32945dd504b25
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-12-06 12:12:23 +0200

    done part1

---
+++ src/cat-api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_pVThY4dIr5gTvYeHoBxFSIzDNo2KPwWO6IF8FKCHH5k0X4bCLe4gN6CG5lPb8OZO';
+
+export function fetchBreeds(url) {
+  return axios(url).then(({ data }) => {
+    return data;
+  });
+}
--- src/index.js
+++ src/index.js
@@ -3,56 +3,47 @@
 //? -------------https://openweathermap.org/-------------
 // import './weatherApi.js';
 
-import SlimSelect from 'slim-select';
-import axios from 'axios';
 import { Notify } from 'notiflix';
-
-axios.defaults.headers.common['x-api-key'] =
-  'live_pVThY4dIr5gTvYeHoBxFSIzDNo2KPwWO6IF8FKCHH5k0X4bCLe4gN6CG5lPb8OZO';
+import { fetchBreeds } from './cat-api';
 
 import { TheCatAPI } from '@thatapicompany/thecatapi';
 
-// .setSelected(axios('https://api.thecatapi.com/v1/breeds'));
+const url = `https://api.thecatapi.com/v1`;
+
+fetchBreeds(url + '/breeds')
+  .then(data => {
+    document.querySelector('.loader-container').innerHTML =
+      "<span class='loader'></span>";
+    let storedBreeds = [];
+    storedBreeds = data;
+
+    for (let i = 0; i < storedBreeds.length; i += 1) {
+      const breed = storedBreeds[i];
+      let option = document.createElement('option');
+
+      const { image, id, name } = breed;
+
+      if (!image) continue;
+
+      option.classList.add('option-item');
+      option.value = `${id}`;
+      option.innerHTML = `${name}`;
+      document.querySelector('.breed-select').appendChild(option);
+    }
+  })
+  .then(() => {
+    setTimeout(() => {
+      document.querySelector('.loader-container').innerHTML = '';
+    }, 500);
+  })
+  .catch(function (error) {
+    Notify.failure(
+      `Oops! Something went wrong! Try reloading the page! ${error}`
+    );
+  });
 
 /*
 Під час завантаження сторінки має виконуватися HTTP-запит за колекцією порід. Для цього необхідно виконати GET-запит на ресурс https://api.thecatapi.com/v1/breeds, що повертає масив об'єктів. У разі успішного запиту, необхідно наповнити select.breed-select опціями так, щоб value опції містило id породи, а в інтерфейсі користувачеві відображалася назва породи.
 
 Напиши функцію fetchBreeds(), яка виконує HTTP-запит і повертає проміс із масивом порід - результатом запиту. Винеси її у файл cat-api.js та зроби іменований експорт.
 */
-
-const url = `https://api.thecatapi.com/v1/breeds`;
-const api_key =
-  'live_pVThY4dIr5gTvYeHoBxFSIzDNo2KPwWO6IF8FKCHH5k0X4bCLe4gN6CG5lPb8OZO';
-let storedBreeds = [];
-
-fetch(url, {
-  headers: {
-    'x-api-key': api_key,
-  },
-})
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    storedBreeds = data;
-    select.setData(data);
-
-    for (let i = 0; i < storedBreeds.length; i += 1) {
-      const breed = storedBreeds[i];
-      let option = document.createElement('option');
-      if (!breed.image) continue;
-      //use the current array index
-
-      option.value = `${breed.id}`;
-      option.innerHTML = `${breed.name}`;
-      document.querySelector('div .breed-select').appendChild(option);
-    }
-  })
-  .catch(function (error) {
-    // document.querySelector('.error').classList.toggle('is-hidden')
-    console.log(error);
-  });
-
-const select = new SlimSelect({
-  select: '.breed-select',
-});
